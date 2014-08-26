0e6aff55697ae9d76f0e215add866a3cbdd39c7c
TomRey <tom.rey1994@gmail.com>
2014-08-26 15:42:22 +0200

    Add and deleting notes
    Adding a menu for deleting and/or adding notes and an activity to add
    notes

---
+++ src/ch/hearc/smarthome/notes/AjouterNote.java
@@ -0,0 +1,71 @@
+package login.p1;
+
+import java.util.Calendar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class AjouterNote extends Activity {
+	
+	public EditText strDestinataire;
+	public EditText strSujet;
+	public EditText strDetail;
+	public String date;
+		
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_ajouter_note);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.ajouter_note, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public void AddNote(View view)
+	{
+		EditText destinataire =(EditText) findViewById(R.id.destinataire);
+		String strDestinataire = destinataire.getText().toString();
+		destinataire.setText("");
+		
+		EditText sujet = (EditText) findViewById(R.id.sujet);
+		String strSujet = sujet.getText().toString();
+		sujet.setText("");
+		
+		EditText detail = (EditText) findViewById(R.id.detail);
+		String strDetail = detail.getText().toString();
+		detail.setText("");
+		
+		Calendar rightNow = Calendar.getInstance();
+        date = rightNow.get(Calendar.DAY_OF_MONTH)+"/"+rightNow.get(Calendar.MONTH)+"/"+rightNow.get(Calendar.YEAR);
+		
+        
+        Intent intent = new Intent(AjouterNote.this, NoteActivity.class);
+		intent.putExtra("destinataire", strDestinataire);
+		intent.putExtra("sujet", strSujet);
+		intent.putExtra("detail", strDetail);
+		intent.putExtra("date", date);
+		
+		startActivity(intent);
+        
+	}
+}
---
+++ src/ch/hearc/smarthome/notes/NoteMenu.java
@@ -0,0 +1,48 @@
+package login.p1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class NoteMenu extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_note_menu);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.note_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+		
+	public void SupprimerNoteActivity(View view)
+	{
+		Intent intent = new Intent(NoteMenu.this, NoteActivity.class);
+		startActivity(intent);
+	}
+	
+	public void AjouterNoteActivity(View view)
+	{
+		Intent intent = new Intent(NoteMenu.this, AjouterNote.class);
+		startActivity(intent);
+	}
+}
