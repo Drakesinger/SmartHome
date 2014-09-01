bcb0fba54f5e42c360053f26c688caf4b16e20ff
Thomas Roulin <thomas.roulin@he-arc.ch>
2014-09-01 10:49:38 +0200

    Remove startActivity redundancy in HomeActivity

--- src/ch/hearc/smarthome/SHHomeActivity.java
+++ src/ch/hearc/smarthome/SHHomeActivity.java
@@ -11,12 +11,10 @@ import ch.hearc.smarthome.networktester.SHBluetoothTesting;
 import ch.hearc.smarthome.notes.NoteMenu;
 
 /** Activity containing the links to our 4 other activities */
-public class SHHomeActivity extends SHBluetoothActivity
-{
+public class SHHomeActivity extends SHBluetoothActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		TextView textView = new TextView(this);
@@ -27,38 +25,21 @@ public class SHHomeActivity extends SHBluetoothActivity
 		setContentView(R.layout.fragment_home);
 	}
 
-	public void DoorMainActivity(View view)
-	{
-		Intent intent = new Intent(this, DoorActivity.class);
-		preventCancel = true;
-		startActivity(intent);
-	}
+	public void startMenu(View v) {
+		Intent i = null;
 
-	public void HeatMainActivity(View view)
-	{
-		Intent intent = new Intent(this, HeatingMainActivity.class);
+		if (v.getId() == R.id.main_door_button) {
+			i = new Intent(this, DoorActivity.class);
+		} else if (v.getId() == R.id.main_heating_button) {
+			i = new Intent(this, HeatingMainActivity.class);
+		} else if (v.getId() == R.id.main_note_button) {
+			i = new Intent(this, NoteMenu.class);
+		} else if (v.getId() == R.id.main_alarm_button) {
+			// nahp
+		} else if (v.getId() == R.id.main_bluetooth_button) {
+			i = new Intent(this, SHBluetoothTesting.class);
+		}
 		preventCancel = true;
-		startActivity(intent);
-	}
-
-	public void NotesMainActivity(View view)
-	{
-		Intent intent = new Intent(this, NoteMenu.class);
-		preventCancel = true;
-		startActivity(intent);
-	}
-
-	public void AlarmMainActivity(View view)
-	{
-		// Intent intent = new Intent(this, AlarmeMainActivity.class);
-		// preventCancel = true;
-		// startActivity(intent);
-	}
-	// LOL
-	public void OnTesteLeBluetoothMagueule(View v)
-	{
-		Intent intent = new Intent(this, SHBluetoothTesting.class);
-		preventCancel = true;
-		startActivity(intent);
+		startActivity(i);
 	}
 }
