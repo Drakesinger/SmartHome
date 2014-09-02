8bc340fa69b3b625af2cd217ce685b7567631af1
Horia Mut <horia.mut@he-arc.ch>
2014-09-02 22:24:08 +0200

    Move Activity List to it's own package

---
+++ src/ch/hearc/smarthome/activitylist/SHActivityListBaseAdapter.java
@@ -0,0 +1,70 @@
+package ch.hearc.smarthome.activitylist;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import ch.hearc.smarthome.R;
+
+public class SHActivityListBaseAdapter extends BaseAdapter
+{
+
+	private static ArrayList<SHActivityC>	actionArrayList;
+
+	private LayoutInflater				mInflater;
+
+	public SHActivityListBaseAdapter(Context context, ArrayList<SHActivityC> results)
+	{
+		actionArrayList = results;
+		mInflater = LayoutInflater.from(context);
+	}
+
+	public int getCount( )
+	{
+		return actionArrayList.size( );
+	}
+
+	public Object getItem(int position)
+	{
+		return actionArrayList.get(position);
+	}
+
+	public long getItemId(int position)
+	{
+		return position;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent)
+	{
+		ViewHolder holder;
+
+		if(convertView == null)
+		{
+			convertView = mInflater.inflate(R.layout.activity_list_activity_view, null);
+			holder = new ViewHolder( );
+			holder.tvAction = (TextView) convertView.findViewById(R.id.al_av_tv_ActivityName);
+			holder.tvDescription = (TextView) convertView.findViewById(R.id.al_av_tv_ActivityDesc);
+
+			convertView.setTag(holder);
+		}
+		else
+		{
+			holder = (ViewHolder) convertView.getTag( );
+		}
+
+		holder.tvAction.setText(actionArrayList.get(position).getCActivity( ));
+		holder.tvDescription.setText(actionArrayList.get(position).getDescription( ));
+
+		return convertView;
+	}
+
+	static class ViewHolder
+	{
+		TextView	tvAction;
+		TextView	tvDescription;
+	}
+}
