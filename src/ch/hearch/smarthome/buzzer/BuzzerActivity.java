b4bdd5f806c4f2b06ccbd7b2a4d028599eb1d4cc
Thomas Roulin <thomas.roulin@he-arc.ch>
2014-09-04 11:28:44 +0200

    To infinite and beyond, not kidding this time

---
+++ src/ch/hearch/smarthome/buzzer/BuzzerActivity.java
@@ -0,0 +1,25 @@
+package ch.hearch.smarthome.buzzer;
+
+import android.os.Bundle;
+import android.view.View;
+import ch.hearc.smarthome.R;
+import ch.hearc.smarthome.bluetooth.SHBluetoothActivity;
+import ch.hearc.smarthome.bluetooth.SHCommunicationProtocol;
+
+public class BuzzerActivity extends SHBluetoothActivity {
+	
+	private static SHCommunicationProtocol protocol;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.buzzer_main);
+		
+		protocol = new SHCommunicationProtocol();
+	}
+	
+	public void toInfiniteAndBeyond(View v){
+		write(protocol.generateDataToSend("to infinite and beyond", " "));
+	}
+}
